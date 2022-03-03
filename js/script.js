5d3510135758d27013af1703a22da6cb157db0e7
giorgios17 <giosur@live.it>
2022-03-03 14:07:40 +0100

    exercise done

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const firstname = prompt('Come ti chiami?')
+const surname = prompt('Qual è il tuo cognome?')
+const prefercolor = prompt('Qual è il tuo colore preferito?')
+
+console.log(firstname + surname + prefercolor + '21')
